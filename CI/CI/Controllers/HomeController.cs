bbf45f9738563165d5bede9cf6dd9cf071e1c6cf
Keveen-ghori <kjghori3@gmail.com>
2023-02-17 18:25:48 +0530

    Mission

---
+++ CI/CI/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("/")]
+        [Route("/Home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+       
+    }
+}
--- Middleware_Demo/Middleware_Demo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Middleware_Demo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
